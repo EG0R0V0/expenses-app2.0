d71cb3abbf14e561487c7eeabf49b176cf773cad
Egorov Roman <hihihi.ru1@mail.ru>
2025-06-03 08:30:46 +0300

    add localStorage limit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let LIMIT = 10000;
+let limit = 10000;
+const GLOBAL_LABEL_LIMIT = "limit";
 const STATUS_IN_LIMIT = 'все хорошо!';
 const STATUS_OUT_OF_LIMIT = 'все плохо!';
 const STATUS_OUT_OF_LIMIT_CLASSNAME = 'status_red';
@@ -15,8 +16,18 @@ const categoryNode = document.getElementById('categorySelect');
 const changeLimitNode = document.getElementById('js-change_limit');
 
 let expenses = [];
-limitNode.innerText = LIMIT;
 statusNode.innerText = STATUS_IN_LIMIT;
+limitNode.innerText = limit;
+
+
+function initLimit() {
+    const limitFromStorage = parseInt(localStorage.getItem(GLOBAL_LABEL_LIMIT));
+    if (!limitFromStorage) {
+        return;
+    }
+    limitNode.innerText = limitFromStorage;
+}
+initLimit();
 
 function getTotal() {
     let sum = 0;
@@ -30,11 +41,11 @@ function renderStatus() {
     const total = getTotal(expenses);
     sumNode.innerText = total;
 
-    if (total <= LIMIT) {
+    if (total <= limit) {
         statusNode.innerText = STATUS_IN_LIMIT;
         statusNode.classList.remove(STATUS_OUT_OF_LIMIT_CLASSNAME);
     } else {
-        statusNode.innerText = (`${STATUS_OUT_OF_LIMIT} (${LIMIT - total} ${CURRENCY})`);
+        statusNode.innerText = (`${STATUS_OUT_OF_LIMIT} (${limit - total} ${CURRENCY})`);
         statusNode.classList.add(STATUS_OUT_OF_LIMIT_CLASSNAME);
     }
 }
@@ -60,11 +71,13 @@ function addBtnHandler() {
     const currentAmont = getExpensesFromUser();
 
     if (!currentAmont) {
+        alert('Введите сумму корректно');
         return
     }
 
     const currentCategory = getCategoryFromUser();
     if (currentCategory === "Категория") {
+        alert("Выберите категорию");
         return
     }
 
@@ -112,7 +125,10 @@ const changeLimitHandler = () => {
 
     limitNode.innerText = newLimitValue;
 
-    LIMIT = newLimitValue;
+    limit = newLimitValue;
+
+    // записываем лимит в local Storage
+    localStorage.setItem('limit', newLimitValue);
 
     render();
 };
